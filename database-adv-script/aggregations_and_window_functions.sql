45f9402ce88ea4f652f9d2e5bfdcd3b7d81176f2
Dodzi Keglo <stephanekeglo@gmail.com>
2025-07-02 14:36:04 +0000

    Practice aggregations and windows functions

---
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -0,0 +1,34 @@
+-- Connect to database
+\c airbnb
+
+-- Find the total number of bookings made by each user, using the COUNT function and GROUP BY clause.
+SELECT
+  users.user_id,
+  users.first_name,
+  users.last_name,
+  COUNT(user_id) total_booking
+FROM
+  users
+  INNER JOIN bookings USING(user_id)
+GROUP BY
+  users.user_id;
+
+-- Use a window function (ROW_NUMBER, RANK) to rank properties based on the total number of bookings they have received.
+SELECT
+  properties.property_id,
+  properties.name,
+  properties.description,
+  properties.location,
+  properties.price_per_night,
+  COUNT(property_id) total_bookings,
+  RANK() OVER (
+    ORDER BY
+      COUNT(price_per_night)
+  )
+FROM
+  properties
+  INNER JOIN bookings USING(property_id)
+GROUP BY
+  (property_id)
+ORDER BY
+  total_bookings DESC
