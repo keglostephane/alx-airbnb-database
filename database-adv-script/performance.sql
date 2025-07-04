9c782d520ec9419f13efc47fd30b01b4f532645d
Dodzi Keglo <stephanekeglo@gmail.com>
2025-07-04 19:21:38 +0000

    using WHERE and AND clausses are not necessary

---
+++ database-adv-script/performance.sql
@@ -0,0 +1,27 @@
+-- Connect to database
+\c airbnb
+
+-- Retrieve all bookings along with the user details, property details and payment details
+-- using WHERE and AND clause are not necessary
+SELECT
+  bookings.booking_id,
+  bookings.start_date,
+  bookings.end_date,
+  bookings.total_price,
+  users.first_name,
+  users.last_name,
+  users.email,
+  properties.name property_name,
+  properties.description property_description,
+  properties.location property_location,
+  payments.payment_method
+FROM
+  bookings
+  INNER JOIN users USING (user_id)
+  INNER JOIN properties USING (property_id)
+  LEFT JOIN payments USING (booking_id)
+ORDER BY
+  total_price DESC;
+
+-- Reduce sorting time of query results when sorting by `total_price`
+CREATE INDEX booking_total_price_idx ON bookings (total_price DESC);
